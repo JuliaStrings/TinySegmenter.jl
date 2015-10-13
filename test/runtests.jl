d70914d57fde03f71bf0e9fcfb5bebb719f51cf6
Michiaki Ariga <chezou+github@gmail.com>
2015-10-13 12:39:32 +0900

    fix #1 Make immutable parameters constant

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
 using TinySegmenter
 using Base.Test
 
-seg = Segmenter()
-@test tokenize(seg, "私の名前は中野です") == ["私", "の", "名前", "は", "中野", "です"]
-@test tokenize(seg, "") == []
+@test tokenize("私の名前は中野です") == ["私", "の", "名前", "は", "中野", "です"]
+@test tokenize("") == []
