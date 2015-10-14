b4831b397273673f8de5a5d05cde0a9e38ce6c1c
Michiaki Ariga <chezou+github@gmail.com>
2015-10-14 23:56:01 +0900

    Add test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using TinySegmenter
 using Base.Test
 
 @test tokenize("私の名前は中野です") == ["私", "の", "名前", "は", "中野", "です"]
+@test tokenize("TinySegmenterは25kBで書かれています。")　== ["TinySegmenter", "は", "2", "5", "kB", "で", "書か", "れ", "て", "い", "ます", "。"]
 @test tokenize("") == []
 #@test _ctype('一') == 'M'
 #@test _ctype('〆') == 'H'
