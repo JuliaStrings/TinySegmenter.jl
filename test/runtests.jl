92bf1565d61440e910325e4549351683f4a1c356
Michiaki Ariga <chezou+github@gmail.com>
2015-10-14 21:58:55 +0900

    Use character instead of regex

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,10 @@ using Base.Test
 
 @test tokenize("私の名前は中野です") == ["私", "の", "名前", "は", "中野", "です"]
 @test tokenize("") == []
+#@test _ctype('一') == 'M'
+#@test _ctype('〆') == 'H'
+#@test _ctype('名') == 'H'
+#@test _ctype('あ') == 'I'
+#@test _ctype('ア') == 'K'
+#@test _ctype('Ｚ') == 'A'
+#@test _ctype('９') == 'N'
