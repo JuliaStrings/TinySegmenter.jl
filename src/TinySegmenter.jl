018d2810d2f6422786b706f2f198249a83d8cbc9
Michiaki Ariga <chezou+github@gmail.com>
2015-10-16 00:49:18 +0900

    Update comments

--- src/TinySegmenter.jl
+++ src/TinySegmenter.jl
@@ -90,8 +90,8 @@ end
     tokenize(text::AbstractString)
 
 Given a `text` string, `tokenize` attempts to segment it into a list
-of words, and in particular tries to segment Japanese Kanji text
-into words ("tokens" or "segments"), using the heuristic TinySegmenter algorithm.
+of words, and in particular tries to segment Japanese text
+into words ("tokens" or "segments"), using the TinySegmenter algorithm.
 It returns an array of substrings consisting of the guessed tokens in
 `text`, in the order that they appear.
 """
