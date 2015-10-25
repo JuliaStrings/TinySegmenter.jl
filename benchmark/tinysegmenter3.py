482a80bfbfcc50803efbac1a7bf143b1a17438ae
Michiaki Ariga <chezou+github@gmail.com>
2015-10-26 07:53:22 +0900

    Add warm up to python 3 benchmarker

--- benchmark/tinysegmenter3.py
+++ benchmark/tinysegmenter3.py
@@ -187,6 +187,7 @@ if __name__ == '__main__':
     f = open('timemachineu8j.txt')
     text = f.read()
     loop = 10
+    tokenize('ウォームアップするぞ')
     with Benchmarker(width=20) as bench:
         @bench("tokenize")
         def _(bm):
